c6f6e9f50cc8c89e0b75cab342c42ac9fa190fc4
Kenneth M. DeTullio <detullio@Mgmail.com>
2023-10-09 13:51:32 +0000

    changes for run

--- BasicSuite.cpp
+++ BasicSuite.cpp
@@ -1,10 +1,25 @@
+#include "ObjectBuilder.hpp"
+
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(SkeletalTestDriver)
 
 BOOST_AUTO_TEST_CASE(TheFirstCase)
 {
-  BOOST_TEST_MESSAGE("This test has run");
+
+  try {
+
+  
+    KDeTLearning::parse();
+
+
+  } catch(std::exception &anException) {
+
+    BOOST_FAIL(anException.what());
+
+  }
+
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
